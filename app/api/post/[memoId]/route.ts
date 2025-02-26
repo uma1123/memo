3bf45be1c5a4c9f3b7a5d378b7cdf54f1b0040ca
uma1123 <yuma1596@gmail.com>
2025-02-27 02:01:18 +0900

    Memoの更新機能を追加し、GETおよびDELETEメソッドのリファクタリングを実施

--- app/api/post/[memoId]/route.ts
+++ app/api/post/[memoId]/route.ts
@@ -1,21 +1,23 @@
 import { NextResponse } from "next/server";
 import { prisma } from "../../../../lib/prismaClient";
 
+//取得
 export async function GET(
   request: Request,
   { params }: { params: { memoId: string } }
 ) {
-  const memoId = params.memoId;
+  const { memoId } = await params;
   const memoDetailData = await prisma.post.findUnique({
     where: {
-      id: parseInt(memoId),
+      id: Number(memoId),
     },
   });
   return NextResponse.json(memoDetailData);
 }
 
+//削除
 export async function DELETE(
-  req: Request,
+  request: Request,
   { params }: { params: { memoId: string } }
 ) {
   const { memoId } = params;
@@ -32,4 +34,24 @@ export async function DELETE(
     return NextResponse.json({ error: "メモの削除に失敗しました" });
   }
   //処理内容がDELETE以外のメソッドの場合
+}
+
+//更新
+export async function PUT(
+  request: Request,
+  { params }: { params: { memoId: string } }
+) {
+  const { memoId } = params;
+  const { title, content } = await request.json(); //リクエストボディから取得
+
+  try {
+    const updateMemo = await prisma.post.update({
+      where: { id: Number(memoId) },
+      data: { title, content },
+    });
+    return NextResponse.json(updateMemo);
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ error: "メモの更新に失敗しました" });
+  }
 }
