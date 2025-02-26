433b991b41b11fd338ec3e3c262307e2846bf71b
uma1123 <yuma1596@gmail.com>
2025-02-26 15:31:11 +0900

    DELETEメソッドでメモを削除するAPIエンドポイントを実装

--- app/api/post/[memoId]/route.ts
+++ app/api/post/[memoId]/route.ts
@@ -13,3 +13,23 @@ export async function GET(
   });
   return NextResponse.json(memoDetailData);
 }
+
+export async function DELETE(
+  req: Request,
+  { params }: { params: { memoId: string } }
+) {
+  const { memoId } = params;
+
+  try {
+    //prisma clientのdeleteメソッドを使って、Postテーブルから指定した」IDのメモを削除する
+    const deleteMemo = await prisma.post.delete({
+      where: { id: Number(memoId) },
+    });
+    //削除したメモのデータを200 OKステータスでJSON形式で返す
+    return NextResponse.json(deleteMemo, { status: 200 });
+  } catch (err) {
+    console.error(err);
+    return NextResponse.json({ error: "メモの削除に失敗しました" });
+  }
+  //処理内容がDELETE以外のメソッドの場合
+}
