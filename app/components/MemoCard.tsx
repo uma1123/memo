d9ff3c420054b469ac1ac2cabf80f8b4f09c4113
uma1123 <yuma1596@gmail.com>
2025-02-26 15:31:20 +0900

    MemoCardコンポーネントに削除ボタンを追加し、メモ削除機能を実装

--- app/components/MemoCard.tsx
+++ app/components/MemoCard.tsx
@@ -8,23 +8,38 @@ import {
 import React from "react";
 import { MemoData } from "../types/types";
 import Link from "next/link";
+import { MdReadMore } from "react-icons/md";
+import { FaRegTrashAlt } from "react-icons/fa";
 
 interface MemoDataProps {
   memoData: MemoData;
+  onDelete: (id: number) => void;
 }
 
-const MemoCard = ({ memoData }: MemoDataProps) => {
+const MemoCard = ({ memoData, onDelete }: MemoDataProps) => {
   const { id, title, content, createdAt } = memoData;
   return (
-    <Card>
+    <Card className="w-100 h-60 rounded-2xl shadow-lg hover:shadow-2xl transition-shadow duration-300 p-4 flex flex-col justify-between">
       <CardHeader>
-        <CardTitle>{title}</CardTitle>
+        <CardTitle className="text-lg font-semibold text-gray-800">
+          {title}
+        </CardTitle>
       </CardHeader>
-      <CardContent>{content}</CardContent>
-      <CardFooter className="flex justify-between"></CardFooter>
-      <Link href={`/memo-posts/${id}`} className="text-blue-800">
-        くわしく
-      </Link>
+      <CardContent className="text-gray-600">{content}</CardContent>
+      <CardFooter className="flex justify-between items-center">
+        <Link
+          href={`/memo-posts/${id}`}
+          className="text-blue-600 hover:text-blue-800 transition"
+        >
+          <MdReadMore size={30} />
+        </Link>
+        <button
+          className="text-red-500 hover:text-red-700 transition"
+          onClick={() => onDelete(id)}
+        >
+          <FaRegTrashAlt size={20} />
+        </button>
+      </CardFooter>
     </Card>
   );
 };
