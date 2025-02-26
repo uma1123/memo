b7554ccc727430e6321db6335f78c754eeab2d91
uma1123 <yuma1596@gmail.com>
2025-02-26 15:31:29 +0900

    MemoCardListコンポーネントに削除機能を追加し、メモ削除時の確認ダイアログを実装

--- app/components/MemoCardlist.tsx
+++ app/components/MemoCardlist.tsx
@@ -1,16 +1,43 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import MemoCard from "./MemoCard";
 import { MemoData } from "../types/types";
+import { useRouter } from "next/navigation";
 
 interface MemoAllDataProps {
   memoAllData: MemoData[];
 }
 
 const MemoCardList = ({ memoAllData }: MemoAllDataProps) => {
+  const [memos, setMemos] = useState(memoAllData);
+  const router = useRouter();
+
+  const handleDelete = async (id: number) => {
+    const confirmed = window.confirm("本当に削除しますか？");
+    if (!confirmed) return;
+
+    try {
+      const response = await fetch(`http://localhost:3000/api/post/${id}`, {
+        method: "DELETE",
+      });
+      if (response.ok) {
+        router.refresh();
+        setMemos((prev) => prev.filter((memo) => memo.id !== id));
+      } else {
+        console.error("削除に失敗しました");
+      }
+    } catch (error) {
+      console.error("エラーが発生しました", error);
+    }
+  };
   return (
     <div className="grid lg:grid-cols-3 px-4 py-4 gap-4">
       {memoAllData.map((memoData: MemoData) => (
-        <MemoCard key={memoData.id} memoData={memoData} />
+        <MemoCard
+          key={memoData.id}
+          memoData={memoData}
+          onDelete={handleDelete}
+        />
       ))}
     </div>
   );
