c5e52c86b9c1c533bfc921671a77087b9cf325ce
uma1123 <yuma1596@gmail.com>
2025-03-21 02:00:58 +0900

    MemoCardコンポーネントにお気に入りトグル機能を追加し、スタイルを改善

--- app/components/MemoCard.tsx
+++ app/components/MemoCard.tsx
@@ -9,24 +9,42 @@ import { MemoData } from "../types/types";
 import Link from "next/link";
 import { CiEdit } from "react-icons/ci";
 import { FaRegTrashAlt } from "react-icons/fa";
+import { FaStar, FaRegStar } from "react-icons/fa";
 
 interface MemoDataProps {
   memoData: MemoData;
   onDelete: (id: number) => void;
   onUpdate: (id: number, title: string, content: string) => void;
+  onFavoriteToggle: (id: number, newState: boolean) => void; // 型定義を追加
 }
 
-const MemoCard = ({ memoData, onDelete }: MemoDataProps) => {
-  const { id, title, content } = memoData;
+const MemoCard = ({ memoData, onDelete, onFavoriteToggle }: MemoDataProps) => {
+  const { id, title, content, isFavorite } = memoData;
 
   return (
-    <Card className="w-100 h-60 rounded-2xl shadow-lg hover:shadow-2xl transition-shadow duration-300 p-4 flex flex-col justify-between">
+    <Card className="w-100 h-60 rounded-2xl shadow-lg hover:shadow-2xl transition-shadow duration-300 p-4 flex flex-col justify-between relative">
       <CardHeader>
-        <CardTitle className="text-lg font-semibold text-gray-800">
+        <CardTitle className="text-lg font-semibold text-gray-800 truncate overflow-hidden">
           {title}
         </CardTitle>
+        <button
+          className="absolute top-10 right-10"
+          onClick={() => onFavoriteToggle(id, !isFavorite)}
+        >
+          {isFavorite ? (
+            <FaStar size={20} className="text-yellow-400" title="お気に入り" />
+          ) : (
+            <FaRegStar
+              size={20}
+              className="text-yellow-400"
+              title="お気に入り"
+            />
+          )}
+        </button>
       </CardHeader>
-      <CardContent className="text-gray-600">{content}</CardContent>
+      <CardContent className="text-gray-600 truncate overflow-hidden">
+        {content}
+      </CardContent>
       <CardFooter className="flex justify-between items-center">
         <Link
           href={`/memo-posts/${id}`}
