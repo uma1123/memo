0c235c27f5f1af72b0534f8bfb780102f5eef875
uma1123 <yuma1596@gmail.com>
2025-03-22 23:51:58 +0900

    GETメソッドからリクエスト引数を削除

--- app/api/post/route.ts
+++ app/api/post/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { prisma } from "../../../lib/prismaClient";
 
-export async function GET(request: Request) {
+export async function GET() {
   const allMemoPost = await prisma.post.findMany();
   return NextResponse.json(allMemoPost);
 }
